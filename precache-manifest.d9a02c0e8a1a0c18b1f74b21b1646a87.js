ab886cef4275f2c418db1de9152ec8f99d64414d
Natalia <nataliarudomyr1@gmail.com>
2020-09-18 22:35:50 +0300

    Updates

---
+++ precache-manifest.d9a02c0e8a1a0c18b1f74b21b1646a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deaf84488960548bce2e4784f36bb791",
+    "url": "/udemy/index.html"
+  },
+  {
+    "revision": "42ebe1d8f1d215bbfbc2",
+    "url": "/udemy/static/css/main.edf186a5.chunk.css"
+  },
+  {
+    "revision": "f5c1fcec160018c8171e",
+    "url": "/udemy/static/js/2.13669a2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemy/static/js/2.13669a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ebe1d8f1d215bbfbc2",
+    "url": "/udemy/static/js/main.4ed458d4.chunk.js"
+  },
+  {
+    "revision": "d77fcd0e56a5d1f4dd62",
+    "url": "/udemy/static/js/runtime-main.4fdf34b9.js"
+  }
+]);
