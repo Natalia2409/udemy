3f255b5952cb1d8b6c52fcd0c73428c4bfc681a1
Natalia <nataliarudomyr1@gmail.com>
2020-09-18 22:30:12 +0300

    udemy

---
+++ src/components/app/app.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './app.css';
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+const App = () => {
+
+    const data = [
+        {
+            label: 'Going to learn React',
+            important: true,
+            id: 'qwqw'
+        }, {
+            label: 'That is so good',
+            important: false,
+            id: 'wewe'
+        }, {
+            label: 'I need a break...',
+            important: false,
+            id: 'erer'
+        }
+    ];
+
+    return(
+        <div className="app">
+            <AppHeader/>
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <PostList posts={data}/>
+            <PostAddForm/>
+        </div>
+    )
+}
+
+export default App;
